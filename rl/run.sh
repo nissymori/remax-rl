759299fa9c22019088b361e469d355476017abd4
Soichiro Nishimori <55940613+nissymori@users.noreply.github.com>
2025-09-27 16:42:17 +0900

    Update run.sh

--- rl/run.sh
+++ rl/run.sh
@@ -2,7 +2,7 @@ wandb_project="report"
 
 for seed in 0 1 2 3 4 5 6 7 8 9; do
     for env in minatar-breakout minatar-asterix minatar-freeway minatar-space_invaders; do
-        python pqn.py env_name=$env seed=$seed wandb_project=$wandb_project num_envs=1024 num_steps=128 num_minibatches=128 num_epochs=3 lambda_=0.8 lr=0.0001
+        python pqn.py env_name=$env seed=$seed wandb_project=$wandb_project num_envs=1024 num_steps=128 num_minibatches=128 num_epochs=3 lambda_=0.8 lr=0.001
         for ent_coef in 0.0 0.01; do
             python ppo_q.py env_name=$env seed=$seed wandb_project=$wandb_project ent_coef=$ent_coef
             python ppo_v.py env_name=$env seed=$seed wandb_project=$wandb_project ent_coef=$ent_coef
